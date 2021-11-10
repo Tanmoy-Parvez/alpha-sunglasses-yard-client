9e181af05a9434fe4c1d8d2d1d4a136eb391ae90
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-10 16:23:20 +0600

    route setup and 404 page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div >
   );
 }
 
