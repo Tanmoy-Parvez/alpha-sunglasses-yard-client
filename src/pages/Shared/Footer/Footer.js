da7f5eb9f0e92598d70cbf1f862c9eab22dd66f0
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-12 21:53:40 +0600

    footer changes added done

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -3,31 +3,29 @@ import logo from '../../../images/logo2.png'
 const Footer = () => {
     return (
         <div className="purple-bg pt-5">
-            <div className="row container text-white mx-auto py-4">
-                <div className="col-md-4 ">
-                    <h6> <i className="fas fa-map-marker-alt fs-5"></i> H#000(0th Floor),Road #00,</h6>
-                    <h6>New DOSH,Mohakhali,Dhaka,Bangladesh</h6>
-                    <img src={logo} alt="" className="ps-5 mt-3" />
+            <div className="row container-fluid text-white mx-auto py-4">
+                <div className="col-md-4 ps-5">
+                    <h6> <i className="fas fa-map-marker-alt fs-5"></i> H#111(5th Floor),Road #10,Mirpur-10</h6>
+                    <h6 className="">Gulshan-2, Dhaka-1212, Bangladesh</h6>
+                    <img src={logo} alt="" className="ps-5 w-50 mb-3" />
                 </div>
                 <div className="col-md-2">
-                    <h4 className="mb-4">Company</h4>
-                    <p>About</p>
-                    <p>Projects</p>
-                    <p>Our Teams</p>
-                    <p>Terms and Conditions</p>
-                    <p>Submit Listing</p>
+                    <h4 className="mb-4">Details</h4>
+                    <h6>About</h6>
+                    <h6>Our Teams</h6>
+                    <h6>Our Policy</h6>
+                    <h6>Terms and Conditions</h6>
                 </div>
                 <div className="col-md-2">
-                    <h4 className="mb-4">Quick Links</h4>
-                    <p>Quick Links</p>
-                    <p>Rentals</p>
-                    <p>Sales</p>
-                    <p>Contact</p>
-                    <p>Our Blogs</p>
+                    <h4 className="mb-4">Quick info</h4>
+                    <h6>Sales</h6>
+                    <h6>Contact</h6>
+                    <h6>Our Blogs</h6>
+                    <h6>Reviews</h6>
                 </div>
                 <div className="col-md-4">
                     <h4>About Us</h4>
-                    <p style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet sit sit consectetur adipisicing elit. Non rerum perferendis labore dolorem eius dolor sed velit dignissimos debitis laboriosam dignissimos debitis laboriosam .</p>
+                    <p className="w-75 pe-4" style={{ textAlign: 'justify' }}>We provide our customers with best of best. Their satisfaction is our main policy.We try to give them best product with good quality and in a cheap rate.</p>
 
                     <i className="fab fa-facebook-square fs-3"></i>
                     <i className="fab fa-instagram-square fs-3 mx-3"></i>
