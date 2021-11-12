3a7f56eeae5f7c353a486ed2811304ead4625a13
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-12 15:08:30 +0600

    buy product done

--- src/pages/PurchaseForm/PurchaseForm.js
+++ src/pages/PurchaseForm/PurchaseForm.js
@@ -6,6 +6,7 @@ import logo from '../../images/logo.png'
 import { Card } from 'react-bootstrap';
 import useAuth from '../../hooks/useAuth';
 import NavigationBar from '../Shared/NavigationBar/NavigationBar';
+import { Link } from 'react-router-dom';
 
 
 const PurchaseForm = () => {
@@ -24,10 +25,10 @@ const PurchaseForm = () => {
         data.status = "Pending";
         data.price = product?.price;
         data.img = product?.img;
-        axios.post("https://traveller-dotcom-server.herokuapp.com/booking", data)
+        axios.post("http://localhost:5000/order", data)
             .then(res => {
                 if (res.data.insertedId) {
-                    alert("😃Tour Successfully Booked!")
+                    alert("😃Product Successfully Ordered!")
                     reset()
                 };
             })
@@ -37,22 +38,25 @@ const PurchaseForm = () => {
         <div className="container my-5 pt-3">
             <NavigationBar />
             <div className="row d-flex align-items-center g-4">
-                <div className="col-md-6">
+                <div className="col-md-6 text-center">
                     <Card className="px-2">
-                        <Card.Img variant="top" src={product?.img} className="p-4" />
+                        <Card.Img variant="top" src={product?.img} className="p-4 img-fluid w-75 mx-auto" />
                         <Card.Body className="text-uppercase text-center">
                             <Card.Title>
-                                <h2 className="purple-text">{product?.name}</h2>
+                                <h3 className="purple-text">{product?.name}</h3>
                             </Card.Title>
                             <Card.Text>
-                                <h5 className="text-secondary my-3">{product?.description}</h5>
+                                <h6 className="text-secondary my-3 w-75 mx-auto">{product?.description}</h6>
                             </Card.Text>
                             <Card.Text>
-                                <h4 className="purple-text fs-5">Price: <del>$100.00</del> ${product?.price}.00  Only</h4>
+                                <h4 className="purple-text fs-5">Price: <del>$100.00</del> ${product?.price}.00 Only</h4>
                             </Card.Text>
                             <h5 className="text-white mt-4 purple-bg py-2">Please Fill the order now form</h5>
                         </Card.Body>
                     </Card>
+                    <Link to="/allProducts">
+                        <button className="btn regular-btn py-2 mt-4">Explore More</button>
+                    </Link>
                 </div>
                 <div className="col-md-6 text-center">
                     <div className="py-5 border rounded-3 mx-auto shadow-lg animate__animated animate__backInDown">
